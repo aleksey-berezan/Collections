0ce0352a7094cf2e1c133acbf43435c81e0d8c5b
Oleksii Berezan <aleksey.berezan@gmail.com>
2016-08-17 00:23:43 -0400

    Initial commit.

---
+++ Collections.Tests/FragmentedEnumerableTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Collections.Tests
+{
+    [TestFixture]
+    public class FragmentedEnumerableTest
+    {
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(5)]
+        [TestCase(11)]
+        [TestCase(12)]
+        [TestCase(13)]
+        [TestCase(14)]
+        [TestCase(100)]
+        [TestCase(1000000)]
+        [TestCase(10000000)]
+        public void TestCase(int itemCount)
+        {
+            int[] expected = Enumerable.Range(0, itemCount)
+                                       .Select((x, i) => i)
+                                       .ToArray();
+            var fragmented = new FragmentedEnumerable<int>(expected, 13);
+            int[] actual = fragmented.ToArray();
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+    }
+}
+
---
+++ Collections/FragmentedEnumerable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    public class FragmentedEnumerable<T> : IEnumerable<T>
+    {
+        private readonly LinkedList<T[]> _buckets;
+        private readonly int _lastIndex;
+
+        public FragmentedEnumerable(IEnumerable<T> items, int bucketSize = 64)
+        {
+            if (bucketSize <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(bucketSize), bucketSize, "must be greater than zero");
+            }
+
+            int index = -1;
+            var buckets = new LinkedList<T[]>();
+            T[] bucket = buckets.AddLast(new T[bucketSize]).Value;
+
+            foreach (var item in items)
+            {
+                index++;
+                if (index >= bucketSize)
+                {
+                    bucket = buckets.AddLast(new T[bucketSize]).Value;
+                    index = 0;
+                }
+
+                bucket[index] = item;
+            }
+
+            _buckets = buckets;
+            _lastIndex = index;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (_buckets.Count == 0)
+            {
+                yield break;
+            }
+
+            T[] lastBucket = _buckets.Last.Value;
+            foreach (T[] bucket in _buckets)
+            {
+                int upperBound = bucket == lastBucket
+                    ? _lastIndex + 1
+                    : bucket.Length;
+                for (int i = 0; i < upperBound; i++)
+                {
+                    yield return bucket[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
